8ece0779231aee116b69796f489bdb5fded2d091
be well <other.mind@gmail.com>
2023-02-27 10:50:38 +0200

    Task 3: :

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,70 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            BankAccount clientAccount = new BankAccount("01-01-2016", 12.5, 1650);
+            /*clientAccount.OpenDate = DateTime.Parse("01-01-2016");
+            clientAccount.InterestRate = 12.5;
+            clientAccount.Deposit = 1650;*/
+                        
+            clientAccount.AccountActiveInfo();
+            clientAccount.CurrentTotalInfo();
+            clientAccount.Profit(8);
+
+            Console.ReadLine();
+        }
+    }
+
+    class BankAccount
+    {
+        public DateTime OpenDate { private get; set; }
+
+        public double InterestRate { private get; set; }
+
+        public double Deposit { private get; set; }
+
+        public BankAccount(string openDate, double interestRate, double deposit) 
+        {
+            OpenDate = DateTime.Parse(openDate);
+            InterestRate = interestRate;
+            Deposit = deposit;
+        }
+
+        private int AccountLifespan()
+        {
+            return (DateTime.Now.Date - OpenDate.Date).Days;
+        }
+
+        public void AccountActiveInfo()
+        {
+            Console.WriteLine($"Your account is active for {AccountLifespan()} days");
+        }
+
+        public void Profit(int years)
+        {
+            double profit = CurrentTotal();
+            int FutureYear = DateTime.Now.AddYears(years).Year;
+            for (int i = DateTime.Now.Year; i < FutureYear; i++)
+            {
+                profit += profit * (InterestRate / 100);
+            }
+
+            Console.WriteLine($"Your profit in {years} years will be ${Math.Round(profit, 2)}");
+        }
+
+        private double CurrentTotal()
+        {
+            double total = Deposit;
+            for (int i = DateTime.Now.Year; i > OpenDate.Year; i--)
+            {
+                total += total * (InterestRate / 100);
+            }
+
+            return total;
+        }
+
+        public void CurrentTotalInfo()
+        {
+            Console.WriteLine($"Your current balance is ${Math.Round(CurrentTotal())}");
         }
     }
 }
